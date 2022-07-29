40c413343dd43f34b7bb6f6045682e4538dafec4
Jose Correa <jorenecp10@gmail.com>
2022-07-29 18:53:30 -0500

    cuarto avance homework1

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -169,7 +169,8 @@ function esPositivo(numero) {
 function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
-  // Tu código:
+  str = str.concat("!")
+  return str
 }
 
 function combinarNombres(nombre, apellido) {
