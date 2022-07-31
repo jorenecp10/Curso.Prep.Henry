b763c928dd6ca3777ae8f826498b53b77633fe7b
Jose Correa <jorenecp10@gmail.com>
2022-07-30 20:57:11 -0500

    2 avance homework 3

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -72,6 +72,8 @@ function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
+  let bol = array.includes(elemento)
+  return bol
 }
 
 
@@ -79,6 +81,13 @@ function agregarNumeros(numeros) {
   // "numeros" debe ser un arreglo de enteros (int/integers)
   // Suma todos los enteros y devuelve el valor
   // Tu código:
+   let suma = 0
+  for (let index = 0; index < numeros.length; index++) {
+    suma= parseInt(numeros[index]+suma)
+    
+    
+  }
+  return suma
 }
 
 
@@ -86,6 +95,17 @@ function promedioResultadosTest(resultadosTest) {
   // "resultadosTest" debe ser una matriz de enteros (int/integers)
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
   // Tu código:
+
+  let promedio=0
+  let suma=0
+
+  resultadosTest.forEach(element => {
+   suma = parseInt(element)+suma
+   promedio = suma/resultadosTest.length
+    
+  });
+
+  return promedio
 }
 
 
@@ -93,6 +113,16 @@ function numeroMasGrande(numeros) {
   // "numeros" debe ser una matriz de enteros (int/integers)
   // Devuelve el número más grande
   // Tu código:
+   let mayor = 0
+
+   for (let index = 0; index < numeros.length; index++) {
+    if (numeros[index]>mayor) {
+      mayor=numeros[index]
+      
+    }
+    
+   }
+   return mayor
 }
 
 
@@ -100,6 +130,26 @@ function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
+ let element=1
+  if (arguments.length===0) {
+    return 0
+    
+  }else if (arguments.length===1) {
+    return arguments[0]
+    
+  }else{
+   
+    for (let index = 0; index < arguments.length; index++) {
+      element = arguments[index]*element;
+      
+    }
+    return element
+  }
+
+  
+
+
+
 }
 
 
@@ -107,6 +157,15 @@ function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
 
+let contador=0
+for (let index = 0; index < arreglo.length; index++) {
+  if (arreglo[index]>18) {
+    contador=contador+1
+    
+  }
+  
+}
+return contador
 }
 
 
@@ -115,6 +174,13 @@ function diaDeLaSemana(numeroDeDia) {
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
   //Escribe tu código aquí   
+
+  if (numeroDeDia===1 || numeroDeDia===7) {
+    return "Es fin de semana"
+    
+  }else{
+    return "Es dia Laboral"
+  }
   
 } 
 
